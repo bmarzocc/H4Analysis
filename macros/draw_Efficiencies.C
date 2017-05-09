0bf4bba9f816f3a0be071d5bbafab3fd144ff8e2
Badder <badder.marzocchi@cern.ch>
2017-05-09 13:12:48 +0200

    final fit on binps

--- macros/draw_Efficiencies.C
+++ macros/draw_Efficiencies.C
@@ -14,35 +14,14 @@ Double_t fitfunc(Double_t *v, Double_t *par) {
   Double_t arg = 0;
   if (par[2] != 0) arg = v[0]/par[2];
   Double_t fitval;
+  Double_t effEmission=par[1]*(1 - 1./(par[3]*log(arg)+1));
   if (arg>1)
-    fitval = (par[0]*(1.- (1./(par[1]*log(arg)+1)))) + par[3]*(1-(par[0]*(1.- (1./(par[1]*log(arg)+1)))));
+    fitval = par[0]*(1-effEmission)+effEmission;
   else
-    fitval = 0.;
+    fitval = par[0];
   
   return fitval;
 }
-
-Double_t fitfunc2(Double_t *v, Double_t *par) {
-  Double_t arg = 0;
-  if (par[2] != 0) arg = v[0]/par[2];
-  Double_t fitval;
-  if (arg>1)
-    fitval = par[0] + (par[1]*(1.- (1./(par[3]*log(arg)+1))));
-  else
-    fitval = 0.;
-  
-  return fitval;
-}
-
-Double_t fitfunc3(Double_t *v, Double_t *par) {
-
-  Double_t fitval;
-  fitval = par[1]/(1. + par[2]*exp(par[3]*(v[0]+par[4]))) + par[0];
- 
-  return fitval;
-}
-
-
 
 void draw_Efficiencies() {
 
@@ -66,7 +45,7 @@ void draw_Efficiencies() {
   binp1->SetMarkerStyle(20);
   binp1->SetMarkerColor(1);
   binp1->SetLineColor(1);
-  binp1->SetLineStyle(1);
+  binp1->SetLineStyle(6);
   binp1->SetLineWidth(2);
   binp1->SetMarkerSize(1.1);
   binp3->SetMarkerStyle(21);
@@ -115,59 +94,75 @@ void draw_Efficiencies() {
   effMib3Corr->SetLineWidth(2);
 
   TF1* f1 = new TF1("f1",fitfunc,0.,2700.,4);
-  f1->SetParameters(1.,1.,1300.,0.);  
+  f1->SetParameters(0.2,1.,600.,2.);  
+  //f1->FixParameter(1,1.);
   f1->SetParLimits(0,0.,1.);
-  f1->SetParLimits(1,0.,1.);
-  f1->SetParLimits(2,1100.,1500.);
-  f1->SetParLimits(3,0.,1.);
-  //binp1->Fit("f1","B");
-
-  /*TF1* f2 = new TF1("f2",fitfunc2,0.,2700.,4);
-  f2->SetParameters(0.5,0.5,500.,0.5,0.5);
-  f2->SetParLimits(0,0.,1.);
-  f2->SetParLimits(1,0.,1.);
-  f2->SetParLimits(2,300.,700.);
-  f2->SetParLimits(3,0.,1.);
-  f2->SetParLimits(4,0.,1.);*/
-  TF1* f2 = new TF1("f2",fitfunc3,0.,2700.,5);
-  f2->SetParameters(1.,1.,1.,1.,1.); 
-  f2->SetParLimits(0,0.,1.); 
-  //f2->SetParLimits(1,-999999.,0.); 
-  f2->SetParLimits(4,-2500.,-1000.); 
-  //binp3->Fit("f2","B");
-  //effMib25Corr_1200->Fit("f2","B");
+  f1->SetParLimits(1,0.,1.5);
+  f1->SetParLimits(2,0.,1000.);
+  f1->SetParLimits(3,0.,30.);
+  f1->SetLineColor(kRed+2);
+  f1->SetLineStyle(2);
+  f1->SetLineWidth(2);
+  binp3->Fit("f1","BR","",650,1500);
+ 
+  TF1* f2 = new TF1("f2",fitfunc,0.,2700.,4);
+  f2->SetParameters(0.,1.,1400.,2.);  
+  f2->FixParameter(0,0.);
+  f2->FixParameter(1,1.);
+  //  f2->SetParLimits(0,0.,1.);
+  //  f2->SetParLimits(1,0.,2.);
+  f2->SetParLimits(2,0.,2000.);
+  f2->SetParLimits(3,0.,40.);
+  f2->SetLineColor(kRed+3);
+  f2->SetLineStyle(4);
+  f2->SetLineWidth(2);
+  effMib25Corr_1200->Fit("f2","BR","",1490,2500);
 
   TF1* f3 = new TF1("f3",fitfunc,0.,2700.,4);
-  f3->SetParameters(1.,1.,300.,0.);  
-  f3->SetParLimits(0,0.,1.);
-  f3->SetParLimits(1,0.,1.);
-  f3->SetParLimits(2,0.,1000.);
-  f3->SetParLimits(3,0.,1.);
-  //binp4->Fit("f3","B");
+  f3->SetParameters(0.,1.,1400.,2.);  
+  f3->FixParameter(0,0.);
+  f3->FixParameter(1,1.);
+  //  f3->SetParLimits(0,0.,1.);
+  //  f3->SetParLimits(1,0.,2.);
+  f3->SetParLimits(2,0.,2000.);
+  f3->SetParLimits(3,0.,40.);
+  f3->SetLineColor(kCyan+2);
+  f3->SetLineStyle(5);
+  f3->SetLineWidth(2);
+  effRm5Corr_1200->Fit("f3","BR","",880,1700);
 
   TF1* f4 = new TF1("f4",fitfunc,0.,2700.,4);
-  f4->SetParameters(1.,1.,300.,0.);  
+  f4->SetParameters(0.,1.,1480.,2.);  
+  //f4->FixParameter(0,0.);
+  f4->FixParameter(1,1.);
   f4->SetParLimits(0,0.,1.);
-  f4->SetParLimits(1,0.,1.);
-  f4->SetParLimits(2,0.,1000.);
-  f4->SetParLimits(3,0.,1.);
-  //effMib25Corr->Fit("f4","B");
+  //f4->SetParLimits(1,0.,2.);
+  f4->SetParLimits(2,0.,2000.);
+  f4->SetParLimits(3,0.,40.);
+  f4->SetLineColor(1);
+  f4->SetLineStyle(6);
+  f4->SetLineWidth(2);
+  binp1->Fit("f4","BR","",1480,2500);
 
-  TF1* f5 = new TF1("f5",fitfunc2,0.,2700.,4);
-  f5->SetParameters(0.5,0.5,1400.,0.5,0.5);
+  TF1* f5 = new TF1("f5",fitfunc,0.,2700.,4);
+  f5->SetParameters(0.,1.,1400.,2.);  
+  //  f5->FixParameter(0,0.);
+  f5->FixParameter(1,1.);
   f5->SetParLimits(0,0.,1.);
-  f5->SetParLimits(1,0.,1.);
-  f5->SetParLimits(2,1300.,1500.);
-  f5->SetParLimits(3,0.,1.);
-  f5->SetParLimits(4,0.,1.);
-  //effMib25Corr_1200->Fit("f5","B");
+  //  f5->SetParLimits(1,0.,2.);
+  f5->SetParLimits(2,0.,2000.);
+  f5->SetParLimits(3,0.,40.);
+  f5->SetLineColor(kGreen+3);
+  f5->SetLineStyle(3);
+  f5->SetLineWidth(2);
+  binp4->Fit("f5","BR","",1480,2500);
 
   // plotting
   setStyle();
 
   //TH2F* H2 = new TH2F("H2","",3200,0.,3200.,100,0.,1.4);
   TH2F* H2 = new TH2F("H2","",2700,0.,2700.,100,0.,1.4);
-  H2->GetXaxis()->SetTitle("MCP-stack bias (V)");
+  H2->GetXaxis()->SetTitle("MCP-emission stage bias (V)");
   H2->GetYaxis()->SetTitle("Efficiency");
   TLine *line = new TLine(0,1,2700,1);
 
@@ -206,12 +201,12 @@ void draw_Efficiencies() {
   FPCanvasStyle(c1);
   H2->Draw();
   line->Draw();
-  binp1->Draw("PCsame");
-  binp3->Draw("PCsame");
-  binp4->Draw("PCsame");
-  effMib25Corr_1200->Draw("PCsame");
+  binp1->Draw("Psame");
+  binp3->Draw("Psame");
+  binp4->Draw("Psame");
+  effMib25Corr_1200->Draw("Psame");
   //effRm8Corr_1200->Draw("PCsame");
-  effRm5Corr_1200->Draw("PCsame");
+  effRm5Corr_1200->Draw("Psame");
   //effZS2Corr->Draw("PCsame");
   //effMib3Corr->Draw("PCsame");
   leg->Draw();
